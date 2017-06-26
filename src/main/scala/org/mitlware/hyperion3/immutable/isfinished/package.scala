ddaa12f1dad3dba2e41fc14b9163dbd60adbba9a
david.r.white@ucl.ac.uk <davidrobertwhite@gmail.com>
2017-06-26 16:25:27 +0100

    Source move

---
+++ src/main/scala/org/mitlware/hyperion3/immutable/isfinished/package.scala
@@ -0,0 +1,18 @@
+package org.mitlware.hyperion3.immutable.isfinished
+
+
+
+/*********
+import org.mitlware.immutable._
+
+package object isfinished {
+
+  def maxIterations[S](maxIter: Long, numIterations: Mutable[Long]) : IsFinished[S,Workspace] = 
+    (x: S, env: Workspace) => {
+      val iter = numIterations.get(env) 
+      ( iter == maxIter,numIterations.set( iter + 1, env ))
+    }
+}
+*********/
+
+// End ///////////////////////////////////////////////////////////////
